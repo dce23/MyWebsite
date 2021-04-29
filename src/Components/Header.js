0104e0b85d13d7ab75682a728ed87d04835d37ce
Eric Lepki <eric.lepki01@utrgv.edu>
2021-04-28 21:27:31 -0500

    Removed name and street variable

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Header extends Component {
   render() {
     if (this.props.data) {
-      var name = this.props.data.name;
+      /*var name = this.props.data.name;*/
       var occupation = this.props.data.occupation;
       var description = this.props.data.description;
       var state = this.props.data.address.state;
@@ -64,7 +64,7 @@ class Header extends Component {
 
         <div className="row banner">
           <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
+            <h1 className="responsive-headline">  </h1>
             <h3>
               I'm a {state} based <span>{occupation}</span>. {description}.
             </h3>
